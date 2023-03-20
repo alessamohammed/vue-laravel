aa266829209828a2178d5ecf40f86ac1d3680993
Mohammed Alessa <m@alessamohammed.com>
2023-03-20 23:56:47 +0300

    init laravel & vue , basic highlight

---
+++ resources/js/App.vue
@@ -0,0 +1,27 @@
+<script>
+import VueSelection from './selection.vue'
+export default {
+  components: {
+    VueSelection
+  },
+  methods: {
+   
+        onShare (text) {
+        console.log('share:', text)
+    },
+
+    onHighlight (text) {
+        console.log('highlight:', text)
+    }
+
+	},
+}
+
+
+</script>
+
+<template>
+    <VueSelection @share="onShare" @highlight="onHighlight">
+        Vue 3 , Laravel 10 with Vite
+</VueSelection>
+</template>
