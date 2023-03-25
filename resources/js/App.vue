8ebfb831dcfa34ae16463d08d6e79e311c24ee41
Mohammed Alessa <m@alessamohammed.com>
2023-03-25 22:41:51 +0300

    selection on same tag

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -10,9 +10,6 @@ export default {
         console.log('share:', text)
     },
 
-    onHighlight (text) {
-        console.log('highlight:', text)
-    }
 
 	},
 }
@@ -21,7 +18,7 @@ export default {
 </script>
 
 <template>
-    <VueSelection @share="onShare" @highlight="onHighlight">
-        Vue 3 , Laravel 10 with Vite
+    <VueSelection @share="onShare" style="margin-top: 20%;">
+       <div id="wholeText"><p>Vue 3 , Laravel 10 with </p>Vite </div>
 </VueSelection>
 </template>
--- resources/js/Selection.vue
+++ resources/js/Selection.vue
@@ -17,19 +17,19 @@
 			</span>
 			<span
 				class="item"
-				@mousedown.prevent="handleAction('highlight')"
+				@mousedown.prevent="highlight()"
 			>
 				Highlight
 			</span>
 			<!-- more buttons here -->
 		</div>
-
-		<!-- insterted text is displayed here -->
-		<slot ref="mySlot"/>
+		<slot ref="mySlot"></slot>
 	</div>
 </template>
 
 <script>
+let selection;
+let selectionRange;
 export default {
 	name: 'VueSelection',
 	data () {
@@ -42,6 +42,8 @@ export default {
 	},
 	computed: {
         highlightableEl () {
+            console.log(this.$slots);
+            console.log(this.$slots.default[0]);
         return this.$refs.mySlot;
             }
         },
@@ -55,21 +57,26 @@ export default {
 
 	methods: {
 		onMouseup () {
-			const selection = window.getSelection()
-			const selectionRange = selection.getRangeAt(0)
-
+			selection = window.getSelection()
+			selectionRange = selection.getRangeAt(0)
             console.log(selection)
 			// startNode is the element that the selection starts in
-			const startNode = selectionRange.startContainer.parentNode
+            console.log(selectionRange);
+			const startNode = upTo(selectionRange.startContainer,"div")
 			// endNode is the element that the selection ends in
-			const endNode = selectionRange.endContainer.parentNode
+			const endNode = upTo(selectionRange.endContainer,"div")
 
+            
 			// if the selected text is not part of the highlightableEl (i.e. <p>)
 			// OR
 			// if startNode !== endNode (i.e. the user selected multiple paragraphs)
 			// Then
 			// Don't show the menu (this selection is invalid)
-			if (!startNode.isSameNode(this.highlightableEl) || !startNode.isSameNode(endNode)) {
+            // ! startNode.isSameNode(this.highlightableEl) ||
+			if (!startNode.isSameNode(endNode)) {
+                console.log(!startNode.isSameNode(this.highlightableEl));
+                console.log(!startNode.isSameNode(endNode))
+                console.log("in the same node")
 				this.showMenu = false
 				return
 			}
@@ -80,6 +87,7 @@ export default {
 			// If width === 0 (i.e. no selection)
 			// Then, hide the menu
 			if (!width) {
+                console.log("in width")
 				this.showMenu = false
 				return
 			}
@@ -96,16 +104,212 @@ export default {
 		handleAction (action) {
 			this.$emit(action, this.selectedText)
 		},
-        onShare (text) {
-        console.log('share:', text)
-    },
+        highlight() {
+            // var inputText = document.getElementById("wholeText");
+            // var innerHTML = inputText.innerHTML;
+            // let innerText = inputText.innerText
+            // let start;
+            // let end;
+            // if (start > end)
+            // {
+            //     start = selectionRange.endOffset;
+            //     end = selectionRange.startOffset;
+            // }
+            // else
+            // {
+            // start = selectionRange.startOffset;
+            // end = selectionRange.endOffset;
+            // }
+            // console.log(selectionRange.startContainer);
+            // innerHTML = innerText.substring(0,start) + "<mark>" + innerText.substring(start,end) + "</mark>" + innerText.substring(end,);
+            // inputText.innerHTML = innerHTML;
 
-    onHighlight (text) {
-        console.log('highlight:', text)
-    }
+            selection = window.getSelection()
+			selectionRange = selection.getRangeAt(0)
+            TestTextHighlighting("Vue");
+
+        }
 
 	},
 }
+function upTo(el, tagName) {
+  tagName = tagName.toLowerCase();
+
+  while (el && el.parentNode) {
+    el = el.parentNode;
+    if (el.tagName && el.tagName.toLowerCase() == tagName) {
+      return el;
+    }
+  }
+
+  // Many DOM methods return null if they don't 
+  // find the element they are searching for
+  // It would be OK to omit the following and just
+  // return undefined
+  return null;
+}
+
+var InstantSearch = {
+
+"highlight": function (container, highlightText)
+{
+    var internalHighlighter = function (options)
+    {
+
+        var id = {
+            container: "container",
+            tokens: "tokens",
+            all: "all",
+            token: "token",
+            className: "className",
+            sensitiveSearch: "sensitiveSearch"
+        },
+        tokens = options[id.tokens],
+        allClassName = options[id.all][id.className],
+        allSensitiveSearch = options[id.all][id.sensitiveSearch];
+
+
+        function checkAndReplace(node, tokenArr, classNameAll, sensitiveSearchAll)
+        {
+            console.log(node);
+            console.log("in check and replace")
+            var nodeVal = node.nodeValue, parentNode = node.parentNode,
+                i, j, curToken, myToken, myClassName, mySensitiveSearch,
+                finalClassName, finalSensitiveSearch,
+                foundIndex, begin, matched, end,
+                textNode, mark, isFirst;
+
+                console.log(tokenArr);
+            for (i = 0, j = tokenArr.length; i < j; i++)
+            {
+                curToken = tokenArr[i];
+                myToken = curToken[id.token];
+                myClassName = curToken[id.className];
+                mySensitiveSearch = curToken[id.sensitiveSearch];
+
+                finalClassName = (classNameAll ? myClassName + " " + classNameAll : myClassName);
+
+                finalSensitiveSearch = (typeof sensitiveSearchAll !== "undefined" ? sensitiveSearchAll : mySensitiveSearch);
+
+                isFirst = true;
+       
+                    // if (finalSensitiveSearch)
+                    //     foundIndex = nodeVal.indexOf(myToken);
+                    // else
+                    //     foundIndex = nodeVal.toLowerCase().indexOf(myToken.toLowerCase());
+                    console.log(parentNode,selectionRange.startContainer.parentElement);
+                    
+                    if (parentNode == selectionRange.startContainer.parentElement)
+                    {
+                        foundIndex = selectionRange.startOffset;
+                        console.log(foundIndex);
+                    }
+                    else
+                    foundIndex = -1;
+
+                    if (foundIndex < 0)
+                    {
+                        if (isFirst)
+                            break;
+
+                        if (nodeVal)
+                        {
+                            textNode = document.createTextNode(nodeVal);
+                            console.log("textnode nodeval", textNode)
+                            parentNode.insertBefore(textNode, node);
+                        } // End if (nodeVal)
+
+                        parentNode.removeChild(node);
+                        break;
+                    } // End if (foundIndex < 0)
+
+                    isFirst = false;
+
+
+                    begin = nodeVal.substring(0, foundIndex);
+                    matched = nodeVal.substr(foundIndex, (selectionRange.endOffset - selectionRange.startOffset) );
+
+                    if (begin)
+                    {
+                        textNode = document.createTextNode(begin);
+                        console.log("textNode begin", textNode)
+                        parentNode.insertBefore(textNode, node);
+                    } // End if (begin)
+                    mark = document.createElement("mark");
+                    mark.className += finalClassName;
+                    console.log(matched);
+                    mark.appendChild(document.createTextNode(matched));
+                    nodeVal = nodeVal.substring(foundIndex + (selectionRange.endOffset - selectionRange.startOffset));
+                    console.log(nodeVal);
+                    node.textContent = nodeVal;
+                    console.log(node);
+                    parentNode.insertBefore(mark, node);
+
+                    console.log(nodeVal);
+   
+
+            } // Next i 
+        }; // End Function checkAndReplace 
+
+        function iterator(p)
+        {
+            if (p === null) return;
+    
+            var children = Array.prototype.slice.call(p.childNodes), i, cur;
+            console.log(children);
+            if (children.length)
+            {
+                for (i = 0; i < children.length; i++)
+                {
+                    cur = children[i];
+                    console.log(cur)
+                    if (cur.nodeType === 3)
+                    {
+                        checkAndReplace(cur, tokens, allClassName, allSensitiveSearch);
+                    }
+                    else if (cur.nodeType === 1)
+                    {
+                        iterator(cur);
+                    }
+                }
+            }
+        }; // End Function iterator
+
+        iterator(options[id.container]);
+    } // End Function highlighter
+    ;
+
+
+    internalHighlighter(
+        {
+            container: container
+            , all:
+                {
+                    className: "highlighter"
+                }
+            , tokens: [
+                {
+                    token: highlightText
+                    , className: "highlight"
+                    , sensitiveSearch: false
+                }
+            ]
+        }
+    ); // End Call internalHighlighter 
+
+} // End Function highlight
+
+};
+
+function TestTextHighlighting(highlightText)
+{
+    console.log("in testtexthighlighting")
+    var container = document.getElementById("wholeText");
+    InstantSearch.highlight(container, highlightText);
+}
+
+
+
 </script>
 
 <style>
